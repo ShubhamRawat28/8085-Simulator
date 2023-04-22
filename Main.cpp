9b9b4f3c38f257373e8384cb9fa87a24534c5921
ShubhamRawat28 <91306245+ShubhamRawat28@users.noreply.github.com>
2023-04-22 11:15:41 +0530

    added data members
    added input file and all the required data members for the 8085

---
+++ Main.cpp
@@ -0,0 +1,31 @@
+#include "header.h"
+class Emulator_8085{
+    map<string, string> Memory;
+    bool flags[8]
+    pair<string,string>registers[4]//Taking as string pair as 4 register pairs are there
+    string accumulator//Main register which will be used for every arithematic operation
+    string pc;
+    string start;
+    vector<string>codeAddress;
+    public :
+        Emulator_8085(){
+            for(int i=0;i<8;i++)    flags[i] = false; //Set all the flags to zero
+            for(int i=0;i<4;i++){
+                registers[i] = {NULL,NULL};
+            }
+            accumulator = NULL;
+            //Set all the register to null in start
+        }
+
+        void input(){
+            cout<<"Enter a VALID START ADDRESS"<<endl;
+            cin>>start;
+            if(!validateAddress(start)){
+                cout<<"The address entered is invalid or reserved by the processor"<<endl<<"Please Enter a valid address"<<endl<<"Program logging off"<<endl;
+                exit(0);
+            }
+            else    codeAddress.push_back(start);
+        }
+
+        
+}
---
+++ functions/validateAddress.cpp
@@ -0,0 +1,13 @@
+#include "header.h";
+
+bool validateAddress(string address){
+    if(address.length()!=4) return false;
+    //Valid address 0000 H to FFFF H.
+    for(int i=0;i<address.length();i++){
+        if((address[i]>='0'&&address[i]<='9')||(address[i]>='A'&&address[i]<='F'))
+            continue;
+        else
+            return false;
+    }
+    return true;
+}
