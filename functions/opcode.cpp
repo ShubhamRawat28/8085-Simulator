cc87d880f521e68a074f6625fac756e207044b95
ShubhamRawat28 <91306245+ShubhamRawat28@users.noreply.github.com>
2023-04-26 10:02:14 +0530

    fixed opcode changes in logic

--- functions/opcode.cpp
+++ functions/opcode.cpp
@@ -1,20 +1,20 @@
 #include "../HeaderFiles/header.h"
 
 int opcode(string instruction){
-    string direct[]={"xyzr","xyz","zysei"};
-    string inDirect[]={};
-    string immediate[]={};
-    string implicit[]={};
+    string oneByte[]={};
+    //Instruction having one byte size
+    string twoByte[]={};
+    //Instruction having two byte size
+    string threeByte[]={};
+    //Instruction having three byte size
     stringstream temp(instruction);
     string opcode;
     getline(temp,opcode,' ');
-    int n = sizeof(direct)/sizeof(direct[0]);
-    for(int i=0;i<n;i++)    if(direct[i]==opcode)   return 1;
-    n = sizeof(inDirect)/sizeof(inDirect[0]);
-    for(int i=0;i<n;i++)    if(inDirect[i]==opcode) return 2;
-    n = sizeof(immediate)/sizeof(immediate[0]);
-    for(int i=0;i<n;i++)    if(immediate[i]==opcode)    return 3;
-    n = sizeof(implicit)/sizeof(implicit[0]);
-    for(int i=0;i<n;i++)    if(implicit[i]==opcode) return 4;
+    int n = sizeof(direct)/sizeof(oneByte[0]);
+    for(int i=0;i<n;i++)    if(oneByte[i]==opcode)   return 1;
+    n = sizeof(inDirect)/sizeof(twoByte[0]);
+    for(int i=0;i<n;i++)    if(twoByte[i]==opcode) return 2;
+    n = sizeof(immediate)/sizeof(threeByte[0]);
+    for(int i=0;i<n;i++)    if(threeByte[i]==opcode)    return 3;
     return 0;
 }
